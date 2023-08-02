7ae065ed7976ffc29b788cd8005fb2e503630624
Ricardo Valente <ricardovalente69@hotmail.com>
2023-08-01 22:41:21 -0300

    Feat: Adicionando tratamento de avisos de incosistencias para campos obrigatorios, vazios ou não mascarados.

--- Spring Boot 3/api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -8,19 +8,19 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        @NotBlank
+        @NotBlank(message = "{nome.notBlank}")
         String nome,
-        @NotBlank
-        @Email
+        @NotBlank(message = "{email.notBlank}")
+        @Email(message = "{email.invalido}")
         String email,
-        @NotBlank
+        @NotBlank(message = "{telefone.notBlank}")
         String telefone,
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+        @NotBlank(message = "{crm.notBlank}")
+        @Pattern(regexp = "\\d{4,6}", message = "{crm.invalido}")
         String crm,
-        @NotNull
+        @NotNull(message = "{especialidade.notNull}")
         Especialidade especialidade,
-        @NotNull
+        @NotNull(message = "{endereco.notNull}")
         @Valid
         DadosEndereco endereco) {
 }
