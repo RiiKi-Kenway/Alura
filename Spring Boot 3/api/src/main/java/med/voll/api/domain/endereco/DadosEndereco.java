66d874c24b3afea25711fc3047bb02ff7ab60e87
Ricardo Valente <ricardovalente69@hotmail.com>
2023-08-09 21:39:08 -0300

    Fix: Ajuste nas properties do medico e do logradouro.

--- Spring Boot 3/api/src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
@@ -4,16 +4,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DadosEndereco(
-        @NotBlank(message = "{logradouro.notBlank}")
+        @NotBlank(message = "{endereco.logradouro.notBlank}")
         String logradouro,
-        @NotBlank(message = "{bairro.notBlank}")
+        @NotBlank(message = "{endereco.bairro.notBlank}")
         String bairro,
-        @NotBlank(message = "{cep.notBlank}")
-        @Pattern(regexp = "\\d{8}", message = "{cep.invalido}")
+        @NotBlank(message = "{endereco.cep.notBlank}")
+        @Pattern(regexp = "\\d{8}", message = "{endereco.cep.invalido}")
         String cep,
-        @NotBlank(message = "{cidade.notBlank}")
+        @NotBlank(message = "{endereco.cidade.notBlank}")
         String cidade,
-        @NotBlank(message = "{uf.notBlank}")
+        @NotBlank(message = "{endereco.uf.notBlank}")
         String uf,
         String complemento,
         String numero) {
