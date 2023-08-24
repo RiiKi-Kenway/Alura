1a014bd39a4aaf645a6eb11bdc73b9700fcc90d8
Ricardo Valente <ricardovalente69@hotmail.com>
2023-08-23 23:58:10 -0300

    Feat: Adicionando role na classe security e em alguns meotodos de listagem.

--- Spring Boot 3/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -63,6 +64,7 @@ public class MedicoController {
   }
 
   @GetMapping("/{medicoid}")
+  @Secured("ROLE_ADMIN")
   public ResponseEntity detalhar(@PathVariable Long medicoid){
 
     var medico = repository.getReferenceById(medicoid);
--- Spring Boot 3/api/src/main/java/med/voll/api/controller/PacienteController.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -61,6 +62,7 @@ public class PacienteController {
     }
 
     @GetMapping("/{pacienteid}")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity detalhar(@PathVariable Long pacienteid){
 
         var paciente = repository.getReferenceById(pacienteid);
