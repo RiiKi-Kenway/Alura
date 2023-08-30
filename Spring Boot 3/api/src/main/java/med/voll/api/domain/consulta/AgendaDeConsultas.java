6bf725c6bc9b80b7e0b93804397a06304a93d13e
Ricardo Valente <ricardovalente69@hotmail.com>
2023-08-30 20:40:01 -0300

    Feat: Adicionando validações para quando o paciente e o médico forem nullas.

--- Spring Boot 3/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,14 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+
+        if(!pacienteRepository.existsById(dados.pacienteid())){
+            throw new ValidacaoException("Id do paciente informado não existe.");
+        }
+
+        if(dados.medicoid() != null && !medicoRepository.existsById(dados.medicoid())){
+            throw new ValidacaoException("Id do médico informado não existe.");
+        }
 
         var paciente = pacienteRepository.findById(dados.pacienteid()).get();
         var medico = medicoRepository.findById(dados.medicoid()).get();
